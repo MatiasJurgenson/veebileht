79407f84531039680b046055a5f5863880a8ade1
MatiasJurgenson <jurgenson.matias@gmail.com>
2025-02-12 18:03:49 +0200

    basic svelte stuff

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	let numbers = $state([1, 2, 3, 4]);
+	let total = $derived(numbers.reduce((t, n) => t + n, 0));
+
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+		console.log($state.snapshot(numbers));
+	}
+
+	$inspect(numbers).with(console.trace);
+</script>
+
+
+
+<p>{numbers.join(' + ')} = {total}</p>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={addNumber}>
+	Add a number
+</button>
