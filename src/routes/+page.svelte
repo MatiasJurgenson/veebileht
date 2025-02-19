ca19ee2b0ce6063d78793aca8395b8bfd468ed4a
MatiasJurgenson <jurgenson.matias@gmail.com>
2025-02-19 18:32:43 +0200

    basic web

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,9 @@
-<script>
-	let numbers = $state([1, 2, 3, 4]);
-	let total = $derived(numbers.reduce((t, n) => t + n, 0));
+<h1>Soon™</h1>
 
-	function addNumber() {
-		numbers.push(numbers.length + 1);
-		console.log($state.snapshot(numbers));
-	}
-
-	$inspect(numbers).with(console.trace);
-</script>
-
-
-
-<p>{numbers.join(' + ')} = {total}</p>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<button onclick={addNumber}>
-	Add a number
-</button>
+<style>
+  h1 {
+	font-family: sans-serif;
+	font-size: 2em;
+	text-align: center;
+  }
+</style>
